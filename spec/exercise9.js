c99d2a47aa7619ae357c5e0e0896fb5ba21bda15
DESKTOP-F5EVBDP\Pensarfeo <dryto25@hotmail.com>
2016-11-13 23:22:12 +0100

    freash recreate master branch

---
+++ spec/exercise9.js
@@ -0,0 +1,13 @@
+import {Profile, Detail}    from '../Exercises/9.js'
+import React         from 'react';
+import { render }    from 'react-dom'
+
+render(
+    <Profile>
+        <Detail detail={{Name: "Pedro"}} />
+        <Detail detail={{Email: "pedro@pedro.pedro"}} />
+        <Detail detail={{Address: "PedroStraat 21, 3000 Pedroland"}}/>
+    </Profile>,
+    document.getElementById('app')
+)
+
